e890367a817f6b41b0c74ed581ccaf5cc0bb4ac7
sberardinelli <35702877+sberardinelli@users.noreply.github.com>
2018-04-11 15:02:10 -0400

    Add files via upload

---
+++ resourcespage_files/comment-reply.js
@@ -0,0 +1,22 @@
+addComment={moveForm:function(d,f,i,c){var m=this,a,h=m.I(d),b=m.I(i),l=m.I("cancel-comment-reply-link"),j=m.I("comment_parent"),k=m.I("comment_post_ID");if(!h||!b||!l||!j){return}m.respondId=i;c=c||false;if(!m.I("wp-temp-form-div")){a=document.createElement("div");a.id="wp-temp-form-div";a.style.display="none";b.parentNode.insertBefore(a,b)}h.parentNode.insertBefore(b,h.nextSibling);if(k&&c){k.value=c}j.value=f;l.style.display="";l.onclick=function(){var n=addComment,e=n.I("wp-temp-form-div"),o=n.I(n.respondId);if(!e||!o){return}n.I("comment_parent").value="0";e.parentNode.insertBefore(o,e);e.parentNode.removeChild(e);this.style.display="none";this.onclick=null;return false};try{m.I("comment").focus()}catch(g){}return false},I:function(a){return document.getElementById(a)}};
+/*
+     FILE ARCHIVED ON 14:32:03 Oct 20, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:16:59 Apr 11, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 512.866 (3)
+  esindex: 0.013
+  captures_list: 536.379
+  CDXLines.iter: 12.547 (3)
+  PetaboxLoader3.datanode: 577.086 (4)
+  exclusion.robots: 0.354
+  exclusion.robots.policy: 0.306
+  RedisCDXSource: 5.779
+  PetaboxLoader3.resolve: 147.069
+  load_resource: 292.041
+*/
---
+++ resourcespage_files/l10n.js
@@ -0,0 +1,22 @@
+function convertEntities(b){var d,a;d=function(c){if(/&[^;]+;/.test(c)){var f=document.createElement("div");f.innerHTML=c;return !f.firstChild?c:f.firstChild.nodeValue}return c};if(typeof b==="string"){return d(b)}else{if(typeof b==="object"){for(a in b){if(typeof b[a]==="string"){b[a]=d(b[a])}}}}return b};
+/*
+     FILE ARCHIVED ON 14:46:21 Oct 20, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:16:59 Apr 11, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 237.507 (3)
+  esindex: 0.007
+  captures_list: 287.892
+  CDXLines.iter: 11.982 (3)
+  PetaboxLoader3.datanode: 221.423 (4)
+  exclusion.robots: 0.212
+  exclusion.robots.policy: 0.185
+  RedisCDXSource: 34.428
+  PetaboxLoader3.resolve: 101.346
+  load_resource: 228.667
+*/
